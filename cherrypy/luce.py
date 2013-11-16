3fe63e112b30b8dd9d01bd0ebcbbe95be84ddf85
shannonturner <shannon.v.turner@gmail.com>
2013-11-16 16:52:28 -0500

    Added luce_api.py

--- cherrypy/luce.py
+++ cherrypy/luce.py
@@ -6,13 +6,27 @@
 
 import cherrypy
 
+class api(object):
+
+    from luce_api import collections as collections
+    collections.exposed = True
+    
+    @cherrypy.expose
+    def search(self, **kwargs):
+        return "search"
+
+
 class Root(object):
+
+    api = api()
 
     from luce_index import luce_index as index
     index.exposed = True
 
     from luce_match import luce_match
     luce_match.exposed = True
+
+
 
 if __name__ == '__main__':
     
