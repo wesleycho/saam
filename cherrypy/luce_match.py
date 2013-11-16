bb06166eb0a4e105a9c1ef249233832e43e5a106
shannonturner <shannon.v.turner@gmail.com>
2013-11-16 15:46:48 -0500

    luce_match.py 2013 Nov 16 3:46 pm

--- cherrypy/luce_match.py
+++ cherrypy/luce_match.py
@@ -16,4 +16,9 @@ def luce_match(self, **kwargs):
     while right_side == left_side:
         right_side = random.randint(1, max_random)
 
-    return "Left side is: {0} <br> Right side is: {1}".format(left_side, right_side)
+    left_request_url = 'http://edan-api.si.edu/metadataService?rows=1&start={0}&wt=json&fq=online_media_type:Images'.format(left_side)
+    right_request_url = 'http://edan-api.si.edu/metadataService?rows=1&start={0}&wt=json&fq=online_media_type:Images'.format(right_side)
+
+        
+
+    return "Left side is: {0} -- {1} <br> Right side is: {2} -- {3}".format(left_side, left_request_url, right_side, right_request_url)
