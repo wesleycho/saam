a251f8b73b0d585427395c0f8d30f078d10f8887
Wesley Cho <wesley.cho@gmail.com>
2013-11-17 15:11:08 -0500

    Activated some links in object view

--- ui/app/scripts/controllers/object.js
+++ ui/app/scripts/controllers/object.js
@@ -5,4 +5,13 @@ angular.module('SmithsonianApp')
     if ($state.current.data) {
       $scope.image = $state.current.data.image;
     }
+
+    $scope.goTo = function (string) {
+      if (string === 'similar') {
+        var browseState = $state.get('browse');
+        browseState.data = {};
+        browseState.data.topic = $scope.image.indexedStructured.topic[0];
+        $state.go('browse');
+      }
+    };
   });
