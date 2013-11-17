53df7059312c048bc07152e56b7374e22e86fa51
Wesley Cho <wesley.cho@gmail.com>
2013-11-17 14:58:19 -0500

    Deleted empty else block in object controller

--- ui/app/scripts/controllers/object.js
+++ ui/app/scripts/controllers/object.js
@@ -2,7 +2,7 @@
 
 angular.module('SmithsonianApp')
   .controller('ObjectCtrl', function ($scope, $state) {
-    console.log($state);
-    $scope.image = $state.current.data.image;
-    console.log($scope.image);
+    if ($state.current.data) {
+      $scope.image = $state.current.data.image;
+    }
   });
