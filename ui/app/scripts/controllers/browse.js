d5fdd8098a626161b5874486b98b64a52efa0ddb
Wesley Cho <wesley.cho@gmail.com>
2013-11-17 12:00:32 -0500

    Attempt #1 on filter for browse view

--- ui/app/scripts/controllers/browse.js
+++ ui/app/scripts/controllers/browse.js
@@ -4,9 +4,29 @@ angular.module('SmithsonianApp')
   .controller('BrowseCtrl', function ($scope, $q, Collection) {
     var itemPage = 0;
     $scope.columns = [];
-    _.each(_.range(0, 6), function (idx) {
-      $scope.columns.push({ images: [] });
-    });
+    $scope.param = {};
+
+    generateColumns();
+
+    $scope.updateFilter = function (filter) {
+      return _.debounce(function (filter) {
+        var promises = [];
+        itemPage = 0;
+
+        _.each(_.range(0, 6), function (idx) {
+          promises.push(requestFilteredItems(filter, itemPage * 60 + idx * 10));
+          if (idx === 5) {
+            itemPage++;
+          }
+        });
+
+        $q.all(promises).then(function (data) {
+          $scope.columns = [];
+          generateColumns();
+          fillColumns(data);
+        });
+      }, 200);
+    };
 
     $scope.requestMoreItems = function () {
       var promises;
@@ -20,17 +40,35 @@ angular.module('SmithsonianApp')
       });
 
       $q.all(promises).then(function (data) {
-        _.each(data, function (actualData, idx) {
-          Array.prototype.push.apply($scope.columns[idx].images, actualData.response.docs);
-        });
+        fillData(data);
       });
     };
 
     function requestItems (startIdx) {
       return Collection.get({
-        q: '',
+        q: $scope.param.filter ? $scope.param.filter : '',
         rows: 10,
         start: startIdx
+      });
+    }
+
+    function requestFilteredItems (filter, startIdx) {
+      return Collection.get({
+        q: filter,
+        rows: 10,
+        start: startIdx
+      });
+    }
+
+    function generateColumns () {
+      _.each(_.range(0, 6), function (idx) {
+        $scope.columns.push({ images: [] });
+      });
+    }
+
+    function fillData (data) {
+      _.each(data, function (actualData, idx) {
+        Array.prototype.push.apply($scope.columns[idx].images, actualData.response.docs);
       });
     }
   });
