f6b23d9aeba106fa0b5861b64871a63f7419be79
Wesley Cho <wesley.cho@gmail.com>
2013-11-17 11:30:12 -0500

    Fixed mock data for image scrolling

--- ui/app/scripts/controllers/browse.js
+++ ui/app/scripts/controllers/browse.js
@@ -23,6 +23,7 @@ angular.module('SmithsonianApp')
         _.each(data, function (actualData, idx) {
           Array.prototype.push.apply($scope.columns[idx].images, actualData.response.docs);
         });
+        console.log($scope.columns);
       });
     };
 
