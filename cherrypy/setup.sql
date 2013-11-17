8870aa977cedc94efb67e83e455e483e00b0aac7
shannonturner <shannon.v.turner@gmail.com>
2013-11-16 21:43:53 -0500

    Added zoom_highscores to setup.sql

--- cherrypy/setup.sql
+++ cherrypy/setup.sql
@@ -8,3 +8,19 @@ title character varying,
 image_url character varying,
 primary key (id)
 );
+
+create table zoom_highscores
+(
+id serial,
+name character varying,
+score int,
+primary key (id)
+);
+
+insert into zoom_highscores
+(name, score)
+values ('Popcorn Breath', 531),
+('A cat walking across a keyboard', 40),
+('Potato brains', 2),
+('Zorbo the Magnificent', 12),
+('Dracula', 70)
