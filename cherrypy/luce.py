1512e8d3393470725df9b80752761c1beb1889df
shannonturner <shannon.v.turner@gmail.com>
2013-11-17 10:30:24 -0500

    Minor bugfixes and improvements 2013 Nov 17 1029a
    Fixed bugs on luce_match.py
    Added alias luce_mash

--- cherrypy/luce.py
+++ cherrypy/luce.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python2.7
 
 # Cherrypy backend for LuceHack
 # 2013 November 16
@@ -26,6 +27,9 @@ class Root(object):
     from luce_match import luce_match
     luce_match.exposed = True
 
+    luce_mash = luce_match
+    luce_mash.exposed = True
+
     from luce_match_score import luce_match_score
     luce_match_score.exposed = True
 
@@ -43,6 +47,7 @@ if __name__ == '__main__':
     cherrypy.config.update({'server.socket_port': 8080,
                             'server.socket_host': '127.0.0.1',
                             'log.screen': True,
+                            'log.error_file': 'site.log'
                             })
 
     conf = {'/static': {'tools.staticdir.on': True,
