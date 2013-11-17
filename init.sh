9c08fc12d1ef053210a2e13f6fcb6cffa21ff726
mtnguyen <minh.9.nguyen@gmail.com>
2013-11-16 19:02:07 -0500

    wrong dir

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Initialize the server for production
 echo "Update UI changes in ui/app and they will be added to cherrypy/static"
-watch cp -rf ui/app cherrypy/static
+watch cp -rf ui/dist/* cherrypy/static
