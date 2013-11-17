35ed6d88536a9e39540b555ca90bb38f5f1b2e6f
Wesley Cho <wesley.cho@gmail.com>
2013-11-16 20:58:54 -0500

    Proper implementation of mock backend

--- ui/app/mock/backend.js
+++ ui/app/mock/backend.js
@@ -2,7 +2,7 @@
 
 angular.module('mockBackend', ['ngMockE2E'])
   .run(function ($httpBackend) {
-    $httpBackend.passThrough(/.*\.html$/);
-    $httpBackend.passThrough(/.*\.css$/);
-    $httpBackend.passThrough(/.*\.js$/);
+    $httpBackend.whenGET(/.*\.html$/).passThrough();
+    $httpBackend.whenGET(/.*\.css$/).passThrough();
+    $httpBackend.whenGET(/.*\.js$/).passThrough();
   });
