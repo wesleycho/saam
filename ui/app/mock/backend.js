a5ba60b3e2c7942fd9f7451fa32d5c88dab958c4
Wesley Cho <wesley.cho@gmail.com>
2013-11-16 21:06:36 -0500

    Added mock data for new york in collection searh

--- ui/app/mock/backend.js
+++ ui/app/mock/backend.js
@@ -1,8 +1,26 @@
 'use strict';
 
 angular.module('mockBackend', ['ngMockE2E'])
-  .run(function ($httpBackend) {
+  .run(function ($http, $httpBackend, route) {
+    var mockData = {};
+
     $httpBackend.whenGET(/.*\.html$/).passThrough();
     $httpBackend.whenGET(/.*\.css$/).passThrough();
     $httpBackend.whenGET(/.*\.js$/).passThrough();
+    $httpBackend.whenGET(/.*\.json$/).passThrough();
+
+    $http.get('mock/data/new_york.json')
+      .success(setData('new_york'));
+
+    function setData (key) {
+      return function (data) {
+        return mockData[key] = data;
+      };
+    }
+
+    $httpBackend.whenGET(route.api + '/collections').respond(function (method, url, data) {
+      var data = angular.fromJson(data);
+
+      return [200, mockData[data.q]];
+    });
   });
