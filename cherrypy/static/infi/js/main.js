91df678fa867c1aac35f9d0d7900c7d8aae455e2
mtnguyen <minh.9.nguyen@gmail.com>
2013-11-17 11:34:15 -0500

    initial magnific popup. next step: add light boxes for all images

--- cherrypy/static/infi/js/main.js
+++ cherrypy/static/infi/js/main.js
@@ -11,7 +11,7 @@
         offset: 2, // Optional, the distance between grid items
         itemWidth: 210 // Optional, the width of a grid item
     };
-    var rows = 10;
+    var rows = 20;
     var start = 0;
 
     /**
@@ -47,8 +47,7 @@
     function loadData() {
         isLoading = true;
         $('#loaderCircle').show();
-        rows = 10;
-        start += 10;
+        start += rows;
         $.ajax({
             url: apiURL,
             dataType: 'json',
@@ -84,7 +83,7 @@
             var descriptiveNonRepeating = docs[i].descriptiveNonRepeating;
             var image = descriptiveNonRepeating.online_media.media[0].content;
             var title = descriptiveNonRepeating.title.content;
-            
+
             html += '<li>';
 
             // Image tag (preview in Wookmark are 200px wide, so we calculate the height based on that).
@@ -105,9 +104,45 @@
     }
     ;
 
+    $('#tiles').magnificPopup({
+        delegate: 'img',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
+
     // Capture scroll event.
     $(document).bind('scroll', onScroll);
 
+    $('.ajax-popup').magnificPopup({
+        type: 'ajax',
+        closeBtnInside: false,
+        callbacks: {
+            ajaxContentAdded: function(e) {
+                var url = "http://americanart.si.edu/images/1935/1935.13.271_1a.jpg";
+
+                // dynamically load image
+                var img = document.createElement("img");
+                img.onload = function(e) {
+                    var container = document.getElementById("imageContainer");
+                    container.appendChild(e.target);
+                }
+                img.setAttribute("src", url);
+            }
+        }
+
+    });
     // Load first data from the API.
     loadData();
 })(jQuery);
